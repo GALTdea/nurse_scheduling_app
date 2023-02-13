b6e0cd6c62ce17f9b31ba45fe4636989f6912a64
Gustavo Valenzuela <GALTdea@gmail.com>
2023-02-13 15:11:48 -0800

    Adds shift creation capability to year model

--- app/models/year.rb
+++ app/models/year.rb
@@ -19,8 +19,14 @@ class Year < ApplicationRecord
     p  start_date = Date.commercial(year, week).beginning_of_week
     p  end_date = Date.commercial(year, week).end_of_week
 
-    Week.create!(year_id: year_object.id, week_number: week, start_date: start_date, end_date: end_date)
-      #  p d = Date.commercial(year, week)
+    week = Week.create!(year_id: year_object.id, week_number: week, start_date: start_date, end_date: end_date)
+      
+    # week.create_shifts
+    (start_date..end_date).each do |date|
+      # Shift.create(period_id: self.id, start_time: date.beginning_of_day, end_time: date.end_of_day)
+      Shift.create!(week_id: week.id, date: date)
+    end
+
     end
   end
 end
